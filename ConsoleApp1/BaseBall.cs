2daaf0bf56fbd8f9cbd75643b3293e6beb57d73d
Md. Masum Billah <md.masum123@gmail.com>
2023-06-17 16:29:59 +0600

    Add project files.

---
+++ ConsoleApp1/BaseBall.cs
@@ -0,0 +1,62 @@
+﻿public class baseball
+{
+
+
+    public int callPoints(string[] ops)
+    {
+        int[] itv = new int[ops.Length];
+        int i = 0;
+        foreach (var op in ops)
+        {
+
+
+            if (op == "C")
+            {
+                itv[i - 1] = 0;
+                i = i - 1;
+            }
+            else if (op == "D")
+                itv[i] = itv[i - 1] * 2;
+            else if (op == "+")
+                itv[i] = itv[i - 2] + itv[i - 1];
+            else
+                itv[i] = Convert.ToInt32(op);
+
+
+
+            if (op != "C")
+                i++;
+
+
+        }
+
+
+        int ss = 0;
+        foreach (var opv in itv)
+        {
+            ss = ss + opv;
+        }
+
+
+
+
+
+        return ss;
+    }
+
+
+    public void getPoint()
+    {
+        //string[] ii = new string[5] { "5", "2", "C", "D", "+" };
+
+        //string[] ii = new string[8] { "5", "-2","4", "C", "D","9", "+","+" };
+        string[] ii = new string[1] { "1" };
+
+        int oo = callPoints(ii);
+
+        Console.WriteLine(oo);
+
+        //return oo;
+    }
+
+}
---
+++ ConsoleApp1/ValueTuple.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Person
+{
+    
+    delegate void MyDelegate(string message);
+    public void Introduction()
+    {
+
+
+        string str = "racecar";
+
+        if (IsPalindrome(str))
+        {
+            Console.WriteLine("The string is a palindrome.");
+        }
+        else
+        {
+            Console.WriteLine("The string is not a palindrome.");
+        }
+
+
+        //MyDelegate myDelegate = MyMethod;
+        //myDelegate += YourMethod;
+
+        //myDelegate("Hello, world!");
+
+        //Console.WriteLine("I am a person.");
+    }
+
+    public static bool IsPalindrome(string str)
+    {
+        int i = 0;
+        int j = str.Length - 1;
+
+        while (i < j)
+        {
+            if (str[i] != str[j])
+            {
+                return false;
+            }
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+
+
+
+    public void MyMethod(string message)
+        {
+            Console.WriteLine("MyMethod: {0}", message);
+        }
+
+    public void YourMethod(string message)
+        {
+            Console.WriteLine("YourMethod: {0}", message);
+        }
+}
