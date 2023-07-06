3fcf1b3a088088a0dc0ad9a0b5a021dd81d75b56
Md. Masum Billah <md.masum123@gmail.com>
2023-07-07 02:16:01 +0600

    linked_list

---
+++ ConsoleApp1/linked_list.cs
@@ -0,0 +1,63 @@
+﻿using System;
+class Node
+{
+    public int data;
+    public Node next;
+    public Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+
+}
+class Solution
+{
+
+    public static Node insert(Node head, int data)
+    {
+        //Complete this method
+
+        Node node = new Node(data);
+
+        if (head == null)
+            head = node;
+        else
+        {
+            Node tt = head;
+            while (tt.next != null)
+            {
+                tt = tt.next;
+            }
+
+            tt.next = node;
+        }
+
+        return node;
+
+        ////end
+
+
+    }
+
+    public static void display(Node head)
+    {
+        Node start = head;
+        while (start != null)
+        {
+            Console.Write(start.data + " ");
+            start = start.next;
+        }
+    }
+    static void CallMethod()
+    {
+
+        Node head = null;
+        int T = Int32.Parse(Console.ReadLine());
+        while (T-- > 0)
+        {
+            int data = Int32.Parse(Console.ReadLine());
+            head = insert(head, data);
+        }
+        display(head);
+    }
+}
