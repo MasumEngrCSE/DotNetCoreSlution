1fad1753bc411a4547f89806b44bf56cde211153
Md. Masum Billah <md.masum123@gmail.com>
2023-07-17 01:07:43 +0600

    BinarySearchTree

---
+++ ConsoleApp1/BinarySearchTree.cs
@@ -0,0 +1,49 @@
+﻿class BinarySearchTree
+{
+    internal void initialCall()
+    {
+        BNode root = new BNode(30);
+        var n1 = new BNode(15);
+        var n2 = new BNode(45);
+        var n3 = new BNode(6);
+        var n4 = new BNode(41);
+        var n5 = new BNode(54);
+
+
+        root.left = n1;
+        root.right = n2;
+        n1.left = n3;
+        n2.left = n4;
+        n2.right = n5;
+
+        var snode = bsearch(root, 41);
+        Console.WriteLine(snode.value);
+
+    }
+
+
+    internal BNode bsearch(BNode root,int value)
+    {
+
+        if(root.value==value) return root;
+        else if(root.value>value) root=bsearch(root.left,value);
+        else if(root.value<value) root=bsearch(root.right,value);
+
+
+        return root;
+    }
+}
+
+
+
+class BNode
+{
+    public int value;
+    public BNode left;
+    public BNode right;
+
+    public BNode(int value)
+    {
+        this.value = value;
+    }
+}
