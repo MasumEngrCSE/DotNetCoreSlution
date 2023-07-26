ed402427167369ddee1ef52837a5cfa45d727f07
Md. Masum Billah <md.masum123@gmail.com>
2023-07-27 02:37:57 +0600

    update

---
+++ ConsoleApp1/climbing-the-leaderboard.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class CLResult
+{
+
+    /*
+     * Complete the 'climbingLeaderboard' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY ranked
+     *  2. INTEGER_ARRAY player
+     */
+
+
+      public List<int> climbingLeaderboard(List<int> ranked, List<int> player)
+    {
+        List<int> rt = new List<int>();
+
+        // Step 1: Use a HashSet for ranked scores to remove duplicates
+        var rankedSet = new HashSet<int>(ranked);
+
+        // Step 2: Create a dictionary to store ranks of each score in ranked list
+        var rankDict = new Dictionary<int, int>();
+        int rank = 1;
+        foreach (var score in rankedSet.OrderByDescending(p => p))
+        {
+            rankDict[score] = rank++;
+        }
+
+        int mindx = rankDict.Count + 1;
+
+        foreach (int i in player)
+        {
+            int index = 0;
+            // Step 3: Find the rank using the dictionary (constant time)
+            if (rankDict.TryGetValue(i, out int playerRank))
+            {
+                index = playerRank;
+            }
+            else if (i < rankedSet.Min())
+            {
+                index = mindx;
+            }
+            else
+            {
+                // Find the rank manually (linear time, but only for elements not in the dictionary)
+                var pr = rankedSet.First(p => p <= i);
+                index = rankDict[pr];
+            }
+
+            rt.Add(index);
+        }
+
+        return rt;
+    }
+
+    public List<int> climbingLeaderboardOld(List<int> ranked, List<int> player)
+    {
+
+        List<int> rt = new List<int>();
+        var qr = ranked.Select(p => p).Distinct().OrderByDescending(p => p).ToList();
+
+        int mindx = qr.Count() + 1;
+
+        foreach (int i in player)
+        {
+            int index = 0;
+            var pr = qr.FirstOrDefault(p => p <= i);
+            if (pr == 0)
+            {
+                index = mindx;
+            }
+            else
+            {
+                index = qr.IndexOf(pr) + 1;
+            }
+
+            rt.Add(index);
+        }
+
+        return rt.OrderByDescending(p => p).ToList();
+    }
+
+}
+
+class ClimbingLeaderboard
+{
+    public void initialCall()
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int rankedCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> ranked = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+        int playerCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> player = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+
+        List<int> result = new CLResult().climbingLeaderboard(ranked, player);
+
+        Console.WriteLine(String.Join("\n", result));
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
