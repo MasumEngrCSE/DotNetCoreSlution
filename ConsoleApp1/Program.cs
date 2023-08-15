d9f6780bf3fcd95f5ff8f894b4dec8f0e796a6e7
Md. Masum Billah <md.masum123@gmail.com>
2023-08-16 02:28:27 +0600

    update

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,46 +5,11 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 
 //Console.WriteLine("Hello");
 
-//var cl = new newyearchaos();
-//cl.initialCall();
+var cl = new SumNSeriesSolution();
+cl.initialCall();
 
 
-var oq = new List<int>() { 1 , 2 , 5, 3, 7 ,8, 6 ,4 };
-var oq2 = new List<int>();
-oq.ForEach(x => oq2.Add(x));
 
-oq2.Sort();
-
-int b = 0, ai = 0, fvi = 0, bc = 0, ch = 0;
-Console.WriteLine(string.Join(" ", oq));
-foreach (var itm in oq)
-{
-    fvi = oq.FindIndex(p => p == itm);
-    ai = oq2.FindIndex(p => p == itm);
-
-    Console.Write($"fv:{itm}");
-    Console.Write($" ai:{ai}");
-    Console.Write($" fvi:{fvi}");
-
-
-    b = ai - fvi;
-
-    if (b > 2)
-    {
-        ch = 1;
-        break;
-    }
-
-    else if (b > 0)
-        bc += b;
-
-    Console.WriteLine($" B:{b}");
-}
-
-if (ch > 0)
-    Console.WriteLine("Too chaotic");
-else
-    Console.WriteLine($"Total B:{bc}");
 
 //string vv = "84 48 13 20 61 20 33 97 34 45 6 63 71 66 24 57 92 74 6 25 51 86 48 15 64 55 77 30 56 53 37 99 9 59 57 61 30 97 50 63 59 62 39 32 34 4 96 51 8 86 10 62 16 55 81 88 71 25 27 78 79 88 92 50 16 8 67 82 67 37 84 3 33 4 78 98 39 64 98 94 24 82 45 3 53 74 96 9 10 94 13 79 15 27 56 66 32 81 77";
 //List<int> a = vv.Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
---
+++ ConsoleApp1/summing-the-n-series.cs
@@ -0,0 +1,201 @@
+﻿using com.sun.swing.@internal.plaf.metal.resources;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SumNSeriesResult
+{
+
+    /*
+     * Complete the 'summingSeries' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    //public decimal summingSeries(decimal n)
+    //{
+    //    if (n == 1) return 1;
+
+    //    return summingSeries(n - 1) + (2 * n - 1);
+    //}
+
+    delegate BigInteger NumberChanger(BigInteger n);
+
+    static int mod = 1000000007;
+    public double summingSeries(double n)
+    {
+        double sum = 0;
+
+        sum= (((n % mod) * (n % mod)) % mod);
+        //sum = ((n * 2)*n)/2;
+
+
+        return sum;
+    }
+
+
+
+    public BigInteger CallSummingSeries(BigInteger n)
+    {
+        if (n == 0) return 0;
+        return CallSummingSeries(n - 1) + (2 * n - 1);
+
+    }
+
+
+
+    public decimal summingSeries2(decimal n)
+    {
+        //n = n - 1;
+        //var nc = Convert.ToDouble(n);
+        if (n == 0) return 0;
+        return summingSeries2(n - 1) + (2 * n - 1);
+        //return summingSeries(n - 1)+ Convert.ToDecimal(Math.Pow(nc, 2) - Math.Pow((nc - 1), 2));
+
+    }
+
+    public long summingSeriesOrg(long n)
+    {
+        long retval = 0;
+
+        //for (int i = 1; i <= n; i++)
+        //{
+        //    //retval += 2 * i - 1;
+        //    retval += Math.Pow(i, 2)- Math.Pow((i-1), 2);
+        //    // retval += CalculatePower(i, 2) - CalculatePower((i - 1), 2);
+        //}
+
+        ///solution:2
+        //object lockObject = new object(); // Used for synchronization
+        //Parallel.For(1, n + 1, i =>
+        //{
+        //    long term = 2 * i - 1;
+        //    lock (lockObject)
+        //    {
+        //        retval += term;
+        //    }
+        //});
+
+        #region Solution:3
+
+        var lst = new List<long>();
+
+        long nr = n;
+        //int l = n.ToString().Length;
+        long rv = 100000000;
+        if (nr > rv)
+        {
+            while (nr > rv)
+            {
+                nr -= rv;
+                lst.Add(rv);
+            }
+
+            if (nr < rv)
+            {
+                lst.Add(nr);
+            }
+        }
+        else
+        {
+            lst.Add(nr);
+        }
+
+
+        //foreach (var item in lst)
+        //{
+
+        //    retval += summingSeries2(item);
+        //}
+
+
+
+        object lockObject = new object(); // Used for synchronization
+        Parallel.For(0, lst.Count, i =>
+        {
+            retval += summingSeries2(lst[i]);
+            //long term = 2 * i - 1;
+            //lock (lockObject)
+            //{
+            //    retval += term;
+            //}
+        });
+
+        #endregion
+        ///
+
+
+
+
+
+
+
+
+        //retval = n*n;
+
+
+        return retval;
+    }
+
+    public long summingSeries2(long n)
+    {
+        long retval = 0;
+
+
+        object lockObject = new object(); // Used for synchronization
+        Parallel.For(1, n + 1, i =>
+        {
+            long term = 2 * i - 1;
+            lock (lockObject)
+            {
+                retval += term;
+            }
+        });
+
+
+        return retval;
+    }
+
+
+    static int CalculatePower(int baseNumber, int exponent)
+    {
+        int result = 1;
+        for (int i = 0; i < exponent; i++)
+        {
+            result *= baseNumber;
+        }
+        return result;
+    }
+
+}
+
+class SumNSeriesSolution
+{
+    public async void initialCall()
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            long n = Convert.ToInt64(Console.ReadLine().Trim());
+
+            double result = new SumNSeriesResult().summingSeries(n);
+
+            Console.WriteLine(result);
+        }
+
+        //textWriter.Flush();
+        //textWriter.Close();
+    }
+}
+
