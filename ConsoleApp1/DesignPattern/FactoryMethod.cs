c31c46377c02ea634e660fa5e53ab189a8373f96
Md. Masum Billah <md.masum123@gmail.com>
2023-08-27 02:15:00 +0600

    FactoryMethod
    FactoryMethod

---
+++ ConsoleApp1/DesignPattern/FactoryMethod.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.DesignPattern
+{
+    // Product class (base class)
+    public abstract class Product
+    {
+        public abstract void DisplayInfo();
+    }
+
+    // Concrete products
+    public class ConcreteProductA : Product
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Product A");
+        }
+    }
+
+    public class ConcreteProductB : Product
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("Product B");
+        }
+    }
+
+    // Creator (Factory) class
+    public abstract class Creator
+    {
+        public abstract Product FactoryMethod();
+    }
+
+    // Concrete creators
+    public class ConcreteCreatorA : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductA();
+        }
+    }
+
+    public class ConcreteCreatorB : Creator
+    {
+        public override Product FactoryMethod()
+        {
+            return new ConcreteProductB();
+        }
+    }
+
+
+
+    /*
+
+    In this example, the Product class defines the interface for products, and the ConcreteProductA and ConcreteProductB classes are implementations of specific products.
+
+The Creator class represents the factory, providing an abstract method FactoryMethod() that the concrete creators (ConcreteCreatorA and ConcreteCreatorB) will implement. This method creates and returns a specific product instance.
+
+Now, let's see how you can use these classes:
+
+        Creator creatorA = new ConcreteCreatorA();
+        Product productA = creatorA.FactoryMethod();
+        productA.DisplayInfo();  // Output: Product A
+
+        Creator creatorB = new ConcreteCreatorB();
+        Product productB = creatorB.FactoryMethod();
+        productB.DisplayInfo();  // Output: Product B
+
+     */
+
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,14 +10,13 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 //cl.initialCall();
 
 
-var cl = new ApplicationTest();
-cl.Run();
+Creator creatorA = new ConcreteCreatorA();
+Product productA = creatorA.FactoryMethod();
+productA.DisplayInfo();  // Output: Product A
 
-var cl2 = new ApplicationTest2();
-cl2.Run();
-
-var cl3 = new ApplicationTest3();
-cl3.Run();
+Creator creatorB = new ConcreteCreatorB();
+Product productB = creatorB.FactoryMethod();
+productB.DisplayInfo();  // Output: Product B
 
 
 //string vv = "84 48 13 20 61 20 33 97 34 45 6 63 71 66 24 57 92 74 6 25 51 86 48 15 64 55 77 30 56 53 37 99 9 59 57 61 30 97 50 63 59 62 39 32 34 4 96 51 8 86 10 62 16 55 81 88 71 25 27 78 79 88 92 50 16 8 67 82 67 37 84 3 33 4 78 98 39 64 98 94 24 82 45 3 53 74 96 9 10 94 13 79 15 27 56 66 32 81 77";
