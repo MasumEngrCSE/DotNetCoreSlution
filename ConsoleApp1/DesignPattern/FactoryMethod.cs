1c69b5a44c66b68d730f1152aca71709342cbc9e
Md. Masum Billah <md.masum123@gmail.com>
2023-08-27 02:32:15 +0600

    Update FactoryMethod.cs

--- ConsoleApp1/DesignPattern/FactoryMethod.cs
+++ ConsoleApp1/DesignPattern/FactoryMethod.cs
@@ -70,6 +70,31 @@ Now, let's see how you can use these classes:
         Product productB = creatorB.FactoryMethod();
         productB.DisplayInfo();  // Output: Product B
 
+
+
+
+    Why using the Factory Method Pattern is generally more beneficial:
+
+    Decoupling: By using the Factory Method Pattern, you decouple the creation of objects from the client code. 
+                This means that the client code doesn't need to know the exact concrete classes it's working with. 
+                It only interacts with the factory's interface. This is especially useful when you have multiple products 
+                and want to ensure that your client code isn't tightly coupled to specific implementations.
+
+    Abstraction: The Factory Method Pattern allows you to abstract the creation process. 
+                It provides a clear separation between the client code and the actual object creation, 
+                making your code more maintainable and adaptable to changes.
+
+    Extensibility: If you later decide to introduce more types of products, 
+                using the Factory Method Pattern makes it easier to add new product types without changing the existing client code. 
+                You can simply create new concrete creators for the new products.
+
+    Testing: When you use the Factory Method Pattern, it's easier to mock or stub the creation of objects during testing. 
+             You can replace the concrete creators with mock creators to isolate the client code during testing.
+
+    Code Reusability: If the creation logic of your products is complex or involves common steps, 
+            the Factory Method Pattern allows you to encapsulate that logic in the creator classes, 
+            making it reusable across different parts of your application.
+
      */
 
 }
