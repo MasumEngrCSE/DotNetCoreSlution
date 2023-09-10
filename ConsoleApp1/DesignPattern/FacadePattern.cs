0a46dc32dbdbd1d518699bed2ef6feba3b7b1701
Md. Masum Billah <md.masum123@gmail.com>
2023-09-11 00:56:50 +0600

    Facade Pattern

---
+++ ConsoleApp1/DesignPattern/FacadePattern.cs
@@ -0,0 +1,136 @@
+﻿#region Facade Pattern
+
+/*
+ 
+The Facade Pattern is a structural pattern that provides a simplified interface to a complex subsystem or set of interfaces. This pattern is useful when you want to hide the complexities of a system and provide a unified interface for clients.
+
+Here's an example of the Facade Pattern:
+
+Suppose we're building a computer system, and there are various complex components like the CPU, RAM, and hard drive. We can create a ComputerFacade that provides a simplified interface for turning on and off the computer.
+ 
+ */
+
+#endregion
+
+using System;
+
+// Subsystem components
+public class CPU
+{
+    public void Start()
+    {
+        Console.WriteLine("CPU started");
+    }
+
+    public void Shutdown()
+    {
+        Console.WriteLine("CPU shut down");
+    }
+}
+
+public class RAM
+{
+    public void Initialize()
+    {
+        Console.WriteLine("RAM initialized");
+    }
+}
+
+public class HardDrive
+{
+    public void ReadData()
+    {
+        Console.WriteLine("Reading data from the hard drive");
+    }
+}
+
+// Facade
+public class ComputerFacade
+{
+    private CPU cpu;
+    private RAM ram;
+    private HardDrive hardDrive;
+
+    public ComputerFacade()
+    {
+        cpu = new CPU();
+        ram = new RAM();
+        hardDrive = new HardDrive();
+    }
+
+    public void Start()
+    {
+        Console.WriteLine("Starting the computer...");
+        cpu.Start();
+        ram.Initialize();
+        hardDrive.ReadData();
+        Console.WriteLine("Computer started successfully.");
+    }
+
+    public void Shutdown()
+    {
+        Console.WriteLine("Shutting down the computer...");
+        cpu.Shutdown();
+        Console.WriteLine("Computer shut down.");
+    }
+}
+
+
+
+#region In this example
+
+/*
+ 
+In this example, we have subsystem components like CPU, RAM, and HardDrive, each with their specific methods. The ComputerFacade class encapsulates the initialization and shutdown process of the computer, providing a simple interface to clients.
+
+Now, let's see how you can use the ComputerFacade:
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ComputerFacade computer = new ComputerFacade();
+
+        // Starting the computer
+        computer.Start();
+
+        Console.WriteLine("\nWorking on computer...\n");
+
+        // Shutting down the computer
+        computer.Shutdown();
+    }
+}
+
+
+ */
+
+
+
+#endregion
+
+
+#region Why Facade Pattern is Necessary:
+
+/*
+ Why Facade Pattern is Necessary:
+
+Simplified Interface: The Facade Pattern is necessary when you want to provide a simplified and unified interface to a complex subsystem. It shields clients from the details of the subsystem.
+
+Complexity Hiding: It hides the complexity of a system, making it easier for clients to interact with it without needing to know the inner workings.
+
+Decoupling: It promotes loose coupling between clients and subsystems. Changes in the subsystem can be isolated within the facade.
+
+Encapsulation: The pattern encapsulates the initialization, configuration, or interaction with subsystem components in a single place.
+
+Improved Maintainability: Facades make the codebase more maintainable by reducing the dependencies between clients and subsystems.
+
+Consistency: It ensures that clients use the subsystem in a consistent manner, preventing potential misuse.
+
+In summary, the Facade Pattern is necessary when you want to simplify the interaction with a complex system or subsystem by 
+providing a unified and easy-to-use interface. It promotes encapsulation, decoupling, and code maintainability while shielding clients from the complexities of the underlying system.
+ 
+ */
+
+
+#endregion
