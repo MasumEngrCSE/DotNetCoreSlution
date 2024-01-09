ddd5508f9d4f150a55b2544dd59814e7b1be2bad
Md. Masum Billah <md.masum123@gmail.com>
2024-01-09 19:08:45 +0600

    Create TestListInitializer.cs

---
+++ ConsoleApp1/Others/TestListInitializer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Others
+{
+    public class TestListInitializer
+    {
+
+        public void ListInitialize()
+        {
+            List<TestCls> list1 = new List<TestCls>()
+        {
+            new TestCls(){ itemName = "A", vCount = 1 },
+            new TestCls(){ itemName = "B", vCount = 2 }
+        };
+        }
+    }
+
+
+
+
+
+    public class TestCls
+    {
+        public string itemName { get; set; }
+        public int vCount { get; set; }
+    }
+}
