e5da83641ca4476a55db1ad1008da9d4e3e12bdf
Md. Masum Billah <md.masum123@gmail.com>
2024-02-27 00:44:37 +0600

    Create OddEvenDif.cs

---
+++ ConsoleApp1/MIU/OddEvenDif.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.MIU
+{
+    internal class OddEvenDif
+    {
+
+
+       public static int f(int[] a)
+        {
+
+            int odd = 0, even = 0;
+
+            //foreach (int i in a)
+            //{
+            //    if(i%2 == 0)
+            //        even += i;
+            //    else odd += i;
+            //}
+
+            for(int i=0;i<a.Length;i++)
+            {
+                if (a[i] % 2 == 0)
+                    even += a[i];
+                else odd += a[i];
+            }
+
+
+            return odd-even;
+        }
+    }
+}
