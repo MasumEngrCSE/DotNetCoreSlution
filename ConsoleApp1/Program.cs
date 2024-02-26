7fe194a8745c579754a8363748d11440ed771d20
Md. Masum Billah <md.masum123@gmail.com>
2024-02-27 00:44:39 +0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using ConsoleApp1;
 using ConsoleApp1.DesignPattern;
 using ConsoleApp1.leetcode;
+using ConsoleApp1.MIU;
 using ConsoleApp1.Others;
 using System.Diagnostics;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -14,11 +15,16 @@ Console.WriteLine("Hello");
 
 
 
+//var rr = new OddEvenDif();
 
 
+//int rv = OddEvenDif.f(new int[] { 3, 3, 4, 4 });
+//int rv = rr.f(new int[] { 3, 3, 4, 4 });
+//int rv=rr.f(new int[]{ 4, 1, 2,3 });
+//int rv=rr.f(new int[]{ });
+//Console.WriteLine(rv.ToString());
+
+char[] rt = RetunLengthCharacters.f(new char[] { 'a', 'b','c' },1,2);
 
 
-
-
-
-
+Console.WriteLine(new string(rt));
