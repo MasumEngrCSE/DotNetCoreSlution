9423a21040d7bdcf1eaf4aa6034633a1f77c792d
Md. Masum Billah <md.masum123@gmail.com>
2024-02-27 00:44:41 +0600

    Create RetunLengthCharacters.cs

---
+++ ConsoleApp1/MIU/RetunLengthCharacters.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.MIU
+{
+    internal class RetunLengthCharacters
+    {
+        public static char[] f(char[] a, int start, int len)
+        {
+            char[] b = new char[len];
+            if (a.Length < len || start < 0 || start+len>a.Length)
+            {
+                return null;
+            }
+
+            for (int i = start,j=0; j < len;i++,j++)
+            {
+                    b[j] = a[i];
+
+            }
+            return b;
+        }
+    }
+}
