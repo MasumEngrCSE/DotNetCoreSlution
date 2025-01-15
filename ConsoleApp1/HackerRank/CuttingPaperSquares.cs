49eca2476cef006bd79c8b5e81b38cc19ee79c73
Md. Masum Billah <md.masum123@gmail.com>
2025-01-15 18:05:03 +0600

    Create CuttingPaperSquares.cs

---
+++ ConsoleApp1/HackerRank/CuttingPaperSquares.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.HackerRank
+{
+    internal class CuttingPaperSquares
+    {
+    
+        public static long solve(int n, int m)
+        {
+            return 0;
+        }
+    }
+}
